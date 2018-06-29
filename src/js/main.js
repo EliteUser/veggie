f495fecc52ba9a0b8514d397aaf47f9a7edfe5a1
Andrey <unknownuser017@gmail.com>
2018-06-29 14:48:44 +0300

    header responsive

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+var menuButtons = document.querySelectorAll(".menu__button");
+var menuTabs = document.querySelectorAll(".menu__tab");
+
+var navButton = document.querySelector(".main-navigation__button");
+var navList = document.querySelector(".main-navigation__list");
+
+navButton.addEventListener("click", function () {
+  navButton.classList.toggle("main-navigation__button--active");
+  navList.classList.toggle("main-navigation__list--show");
+});
+
+for (var i = 0; i < menuButtons.length; i++) {
+  clickControl(menuButtons[i], menuTabs[i]);
+}
+
+function clickControl(control, slide) {
+  control.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    toggleSlide(control, slide);
+  });
+}
+
+function toggleSlide(control, slide) {
+  for (var i = 0; i < menuButtons.length; i++) {
+    menuButtons[i].classList.remove("menu__button--active");
+    menuTabs[i].classList.remove("menu__tab--active");
+  }
+
+  control.classList.add("menu__button--active");
+  slide.classList.add("menu__tab--active");
+}
